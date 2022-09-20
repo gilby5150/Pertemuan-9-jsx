aea2d5f30335a500866896aa5e3b1156133e7602
Gilby <gilby5150@gmail.com>
2022-09-20 09:51:10 +0700

    pertemuan 9 jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
 import Nav from './components/layout/Navbar'
+import User from './components/user'
 import gilby from './components/image/gilby.jpg'
 
 function App() {
+  // const date = new Date();
+  // const time = date.toLocaleTimeString();
   return (
       <div className="App">
       <header className="App-header">
       <Nav/>
         </header>
         <body className='App-body'>
-        <img src={gilby} className="App-logo" alt="logo" />
+        {/* <img src={gilby} className="App-logo" alt="logo" />
         <h1>Bootcamp batch 3</h1>
         <p>
           Gilby Faqih
-        </p>
-          
+        </p> */}
+          <h1>{new Date().toLocaleTimeString()}</h1>
+          <input type="number" max={5} style={{border:"3px solid", color:"blue"}}/>
+          <User name="Gilby" umur="21"/>
         </body>
         <footer className='App-footer'></footer>
     </div>
---
+++ src/components/user.jsx
@@ -0,0 +1,10 @@
+const User = ({name,umur})=>{
+    return (
+        <div>
+            <h1>Name : {name}</h1>
+            <span>Age : {umur}</span>            
+        </div>
+    )
+}
+
+export default User;
